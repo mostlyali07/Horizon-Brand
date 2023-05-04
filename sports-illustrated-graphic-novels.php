dc9c532654f395d0dbeb43a88d3ef2a271125702
Syedusaama <68234611+Syedusaama@users.noreply.github.com>
2023-05-05 04:00:09 +0500

    update

---
+++ sports-illustrated-graphic-novels.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include "./includes/head.php"?>  
+    <title>Horizon Publishing</title>
+</head>
+
+<body>
+<?php include "./includes/header.php"?>
+    <!---------------- First Section Start ---------------->
+    <?php include "./includes/banner.php"?>
+
+    <!---------------- First Section End ---------------->
+
+    <!---------------- Second Section Start ---------------->
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-2"></div>
+    <div class="col-md-8 singup-clr px-5">
+        <h2 class="text-clrr text-center py-4">SIGN UP TO GET FREE CONSULTANCY</h2>
+        <form class="row g-4">
+            <div class="col-md-6">
+                            <input type="text" class="form-control f-clr" id="first-name" placeholder="First Name" required>
+                        </div>
+                        <div class="col-6">
+                            <input type="tel" class="form-control f-clr" id="phone-number" placeholder="Phone Number" required>
+                        </div>
+                        <div class="col-md-6">
+                            <input type="email" class="form-control f-clr" id="your-email" placeholder="Email" required>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" class="form-control f-clr" id="website-url" placeholder="Website">
+                        </div>
+                        <div class="col-md-6">
+                            <select id="services" class="form-select f-clr" >
+                                <option disabled value selected>Desired Services</option>
+                                <option>Search Engine Optimization (SEO)</option>
+                                <option>Google Ads Management</option>
+                                <option>Social Media Marketing</option>
+                                <option>Website Design & Development</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control f-clr" id="publication-date" placeholder="General Genre Category">
+                        </div>
+
+                        <div class="col-2 text-end">
+                            <button type="submit" class="btns-signup">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-2"></div>
+        </div>
+        </div>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-6 right-side-panel d-flex align-items-center">
+                <div>
+                    <h2 class="font-h2">
+                        STEP INTO A WORLD OF<br> POSSIBILITY WITH OUR BOOK<br> PUBLISHING SERVICES.
+                    </h2>
+                    <p>
+Our book publishing services are enough to make it possible for your manuscript to become a published & even a best seller book. Collaborate with our book publishing specialists for an optimal book publishing plan tailored for your book.
+                    </p>
+
+                    <button class="btns-three" type="submit">READ MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+
+                </div>
+
+            </div>
+            <div class="col-md-6 text-center">
+                <img src="./img/double-book.png" alt="double-book-img" class="Images-main">
+            </div>
+        </div>
+    </div>
+    <!-- <div class="container my-5">
+        <div class="row ">
+            <div class="col-md-12 text-center">
+                <h2 class="font-h2">FROM MANUSCRIPT TO BEST SELLER</h2>
+                <p class="font-p">#1Amazon book marketing team at Horizon marketing & prornotion facilitate you with all<br>
+                    services your book require to become a best sel let.</p>
+            </div>
+
+        </div>
+        <div class="container col-md-12 text-center">
+            <div class="image ">
+                <img src="./img/service1.png" alt="Image">
+            </div>
+            <div class="image">
+                <img src="./img/service2.png" alt="Image">
+            </div>
+            <div class="image">
+                <img src="./img/service3.png" alt="Image">
+            </div>
+            <div class="image">
+                <img src="./img/service4.png" alt="Image">
+            </div>
+            <p class="font-p">Are you ready to become a best-selling author?</p>
+            <button class="btns-three" type="submit">READ MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+        </div>
+    </div> -->
+    <!---------------- Second Section End ---------------->
+
+    <!---------------- Third Section Start ---------------->
+    <div class="back-color">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-10 newsl-form">
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-8">
+                            <p>
+                                IS YOUR BOOK THE NEXT BESTSELLER?
+                            </p>
+                            <h2>
+                                Get a free consult
+                            </h2>
+                            <p>
+                                This is the only way to find out
+                            </p>
+                        
+                            <form class="row form-inline">
+                                  
+                                        <div class="col-md-3">
+                                            <input type="text" class="form-control inlinefm" id="inlineFormName" placeholder="Name">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <input type="email" class="form-control inlinefm" id="inlineFormEmail" placeholder="Email">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <input type="text" class="form-control inlinefm" id="inlineFormNumber" placeholder="Number">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <button type="submit" class="btns-inlinef"><i class="fa-solid fa-paper-plane a-clr"></i></button>
+                                        </div>
+
+                            </form>
+                        </div>
+                        <!-- <div class="col-md-2"></div> -->
+                    </div>     
+                </div>
+                <div class="col-md-1">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!---------------- Third Section End ---------------->
+    <!---------------- Fourth Section Start ---------------->
+    <!-- <div class="gallery-main">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="font-h2">CHECK OUR PORTFOLIO</h2>
+                <p class="font-p">Best sellers supported by our personalized marketing and promotional campaigns.</p>
+            </div>
+        </div>
+    </div>
+        <div>
+            <div class="gallery">
+                <img class="img-s" src="./img/book-red.png" alt="">
+                <img class="img-s" src="./img/book-brown.png" alt="">
+                <img class="img-s" src="./img/book-blue.png" alt="">
+            </div>
+            <div class="gallery">
+                <img class="img-s" src="./img/book-tea.png" alt="">
+                <img class="img-s" src="./img/book-green.png" alt="">
+                <img class="img-s" src="./img/book-grey.png" alt="">
+
+            </div>
+        </div>
+        <div class="container text-center p-5">
+            <p class="font-p">We help generate a buzz among for your Our rnarketing includes tours, book video trailers,<br>
+                social media campaigns, bmk signings and other promoti onal activities.</p>
+            <button class="btns-three" type="submit">VIEW MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+        </div>
+    </div> -->
+    <!---------------- Fourth Section End ---------------->
+
+    <!---------------- Fifth Section End ---------------->
+    <!-- <div class="video-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center ">
+                    <div class="youtube-back">
+                        <video autoplay controls poster="./img/poster.png" class="youtube">
+                            <source src="./img/Trailer.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- <div class="container text-center p-3">
+        <p class="font-p">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been<br>
+            the industry's standard dummy text ever since the 1500s.</p>
+        <button class="btns-three" type="submit">VIEW MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+    </div> -->
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-6 text-center">
+                <img src="./img/audio-book.png" alt="double-book-img" class="Images-main">
+            </div>
+            <div class="col-md-6 right-side-panel d-flex align-items-center">
+                <div>
+                    <h2 class="font-h2">
+                        Audio Book Publishing
+                    </h2>
+                    <p class="font-p">
+                        Enjoy working with a team of professionals for your audio book
+                        narration, audible audio book creation, audio book publishing on
+                        Amazon.<br><br />
+                        Our audio book consultants help you learn the process from
+                        audiobook narration, selection of voiceover till the publishing &
+                        distribution.<br><br />
+                        Distribute your audiobook globally using our audiobook publishing plans
+                    </p>
+                    <button class="btns-three" type="submit">VIEW PLANS &nbsp;  <i class="fa-solid fa-book-open"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!---------------- Fifth Section End ---------------->
+
+<!---------------- Sixth Section Start ---------------->
+<?php include "./includes/carousel.php"?>
+
+<!---------------- Sixth Section End ----------------> 
+
+
+<!---------------- Seventh Section Start ---------------->
+
+
+<!---------------- Seventh Section End ---------------->
+
+
+<!---------------- Eighth Section Start ---------------->
+
+<!---------------- Eighth Section End ---------------->
+
+    <!---------------- Footer Section Start ---------------->
+    <?php include "./includes/footer.php"?> 
+</body>
+</html>
