050cd23466eb6e596a7faf279b20bb63364d6490
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-07-20 17:38:50 +0500

    update first template

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <html lang="en">
 
 <head>
-    <?php include "./includes/head.php"?>  
+    <?php include "./includes/head.php" ?>
     <title>Horizon Publishing</title>
 </head>
 
 <body>
-<?php include "./includes/header.php"?>
+    <?php include "./includes/header.php" ?>
     <!---------------- First Section Start ---------------->
     <div class="main-banner">
         <div class="container">
             <div class="row">
                 <div class="col-md-6 banner-left d-flex align-items-center">
                     <div>
-                        <h1> 
-                            <span style="color: #f58b01;"> BOOK PROMOTION</span> TO REACH THE MAXIMUM 
-                            <span style="color: #f58b01;">HORIZON</span><br>  WITH OUR BOOK<span style="color: #f58b01;"> MARKETING SERVICES.</span>
+                        <h1>
+                            <span style="color: #f58b01;"> BOOK PROMOTION</span> TO REACH THE MAXIMUM
+                            <span style="color: #f58b01;">HORIZON</span><br> WITH OUR BOOK<span style="color: #f58b01;"> MARKETING SERVICES.</span>
                         </h1>
                         <p>
                             Successful book marketing takes time, effort, and dedication. By utilizing a variety of marketing strategies and consistent effort, we can increase your book's visibility and reach the horizon of success.
@@ -34,45 +34,45 @@
     <!---------------- First Section End ---------------->
 
     <!---------------- Second Section Start ---------------->
-        <div class="container ">
-            <div class="row">
-                <div class="col-md-2"></div>
-    <div class="col-md-8 singup-clr px-5">
-        <h2 class="text-clrr text-center py-4">SIGN UP TO GET FREE CONSULTANCY</h2>
-        <form class="row g-4">
-            <div class="col-md-6">
-                            <input type="text" class="form-control f-clr" id="first-name" placeholder="First Name" required>
-                        </div>
-                        <div class="col-6">
-                            <input type="tel" class="form-control f-clr" id="phone-number" placeholder="Phone Number" required>
-                        </div>
-                        <div class="col-md-6">
-                            <input type="email" class="form-control f-clr" id="your-email" placeholder="Email" required>
-                        </div>
-                        <div class="col-6">
-                            <input type="text" class="form-control f-clr" id="website-url" placeholder="Website">
-                        </div>
-                        <div class="col-md-6">
-                            <select id="services" class="form-select f-clr" >
-                                <option disabled value selected>Desired Services</option>
-                                <option>Search Engine Optimization (SEO)</option>
-                                <option>Google Ads Management</option>
-                                <option>Social Media Marketing</option>
-                                <option>Website Design & Development</option>
-                            </select>
-                        </div>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control f-clr" id="publication-date" placeholder="General Genre Category">
-                        </div>
+    <div class="container ">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 singup-clr px-5">
+                <h2 class="text-clrr text-center py-4">SIGN UP TO GET FREE CONSULTANCY</h2>
+                <form class="row g-4">
+                    <div class="col-md-6">
+                        <input type="text" class="form-control f-clr" id="first-name" placeholder="First Name" required>
+                    </div>
+                    <div class="col-6">
+                        <input type="tel" class="form-control f-clr" id="phone-number" placeholder="Phone Number" required>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="email" class="form-control f-clr" id="your-email" placeholder="Email" required>
+                    </div>
+                    <div class="col-6">
+                        <input type="text" class="form-control f-clr" id="website-url" placeholder="Website">
+                    </div>
+                    <div class="col-md-6">
+                        <select id="services" class="form-select f-clr">
+                            <option disabled value selected>Desired Services</option>
+                            <option>Search Engine Optimization (SEO)</option>
+                            <option>Google Ads Management</option>
+                            <option>Social Media Marketing</option>
+                            <option>Website Design & Development</option>
+                        </select>
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control f-clr" id="publication-date" placeholder="General Genre Category">
+                    </div>
 
-                        <div class="col-2 text-end">
-                            <button type="submit" class="btns-signup">SUBMIT</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-md-2"></div>
+                    <div class="col-2 text-end">
+                        <button type="submit" class="btns-signup">SUBMIT</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-2"></div>
         </div>
-        </div>
+    </div>
     <div class="container py-5">
         <div class="row">
             <div class="col-md-6 text-center">
@@ -84,10 +84,10 @@
                         STEP INTO A WORLD OF<br> POSSIBILITY WITH OUR BOOK<br> PUBLISHING SERVICES.
                     </h2>
                     <p>
-Our book publishing services are enough to make it possible for your manuscript to become a published & even a best seller book. Collaborate with our book publishing specialists for an optimal book publishing plan tailored for your book.
+                        Our book publishing services are enough to make it possible for your manuscript to become a published & even a best seller book. Collaborate with our book publishing specialists for an optimal book publishing plan tailored for your book.
                     </p>
 
-                    <button class="btns-three" type="submit">READ MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+                    <button class="btns-three" type="submit">READ MORE&nbsp; <i class="fa-solid fa-book-open"></i></button>
 
                 </div>
 
@@ -118,7 +118,7 @@ Our book publishing services are enough to make it possible for your manuscript 
                 <img class="service-img" src="./img/service4.png" alt="Image">
             </div>
             <p class="font-p">Are you ready to become a best-selling author?</p>
-            <button class="btns-three" type="submit">READ MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+            <button class="btns-three" type="submit">READ MORE&nbsp; <i class="fa-solid fa-book-open"></i></button>
         </div>
     </div>
     <!---------------- Second Section End ---------------->
@@ -142,26 +142,26 @@ Our book publishing services are enough to make it possible for your manuscript 
                             <p>
                                 This is the only way to find out
                             </p>
-                        
+
                             <form class="row form-inline">
-                                  
-                                        <div class="col-md-3">
-                                            <input type="text" class="form-control inlinefm" id="inlineFormName" placeholder="Name">
-                                        </div>
-                                        <div class="col-md-3">
-                                            <input type="email" class="form-control inlinefm" id="inlineFormEmail" placeholder="Email">
-                                        </div>
-                                        <div class="col-md-3">
-                                            <input type="text" class="form-control inlinefm" id="inlineFormNumber" placeholder="Number">
-                                        </div>
-                                        <div class="col-md-3">
-                                            <button type="submit" class="btns-inlinef"><i class="fa-solid fa-paper-plane a-clr"></i></button>
-                                        </div>
+
+                                <div class="col-md-3">
+                                    <input type="text" class="form-control inlinefm" id="inlineFormName" placeholder="Name">
+                                </div>
+                                <div class="col-md-3">
+                                    <input type="email" class="form-control inlinefm" id="inlineFormEmail" placeholder="Email">
+                                </div>
+                                <div class="col-md-3">
+                                    <input type="text" class="form-control inlinefm" id="inlineFormNumber" placeholder="Number">
+                                </div>
+                                <div class="col-md-3">
+                                    <button type="submit" class="btns-inlinef"><i class="fa-solid fa-paper-plane a-clr"></i></button>
+                                </div>
 
                             </form>
                         </div>
                         <!-- <div class="col-md-2"></div> -->
-                    </div>     
+                    </div>
                 </div>
                 <div class="col-md-1">
                 </div>
@@ -172,13 +172,13 @@ Our book publishing services are enough to make it possible for your manuscript 
     <!---------------- Fourth Section Start ---------------->
     <div class="gallery-main">
         <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <h2 class="font-h2">CHECK OUR PORTFOLIO</h2>
-                <p class="font-p">Best sellers supported by our personalized marketing and promotional campaigns.</p>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2 class="font-h2">CHECK OUR PORTFOLIO</h2>
+                    <p class="font-p">Best sellers supported by our personalized marketing and promotional campaigns.</p>
+                </div>
             </div>
         </div>
-    </div>
         <div>
             <div class="gallery">
                 <img class="img-s" src="./img/book-red.png" alt="">
@@ -195,7 +195,7 @@ Our book publishing services are enough to make it possible for your manuscript 
         <div class="container text-center p-5">
             <p class="font-p">We help generate a buzz among for your Our rnarketing includes tours, book video trailers,<br>
                 social media campaigns, bmk signings and other promoti onal activities.</p>
-            <button class="btns-three" type="submit">VIEW MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button>
+            <button class="btns-three" type="submit">VIEW MORE&nbsp; <i class="fa-solid fa-book-open"></i></button>
         </div>
     </div>
     <!---------------- Fourth Section End ---------------->
@@ -211,17 +211,17 @@ Our book publishing services are enough to make it possible for your manuscript 
                             Your browser does not support the video tag.
                         </video>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
     </div>
     <div class="container text-center p-3">
-    <h2 class="font-h2">VIDEO TRAILERS</h2>
+        <h2 class="font-h2">VIDEO TRAILERS</h2>
         <p class="font-p">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been<br>
-            the industry's standard dummy text ever since the 1500s.</p>
-        <!-- <button class="btns-three" type="submit">VIEW MORE&nbsp;  <i class="fa-solid fa-book-open"></i></button> -->
+            the industry's standard dummy text ever since the 1500s.
+        </p>
     </div>
     <div class="container py-5">
         <div class="row">
@@ -242,31 +242,16 @@ Our book publishing services are enough to make it possible for your manuscript 
                         distribution.<br><br />
                         Distribute your audiobook globally using our audiobook publishing plans
                     </p>
-                    <button class="btns-three" type="submit">VIEW PLANS &nbsp;  <i class="fa-solid fa-book-open"></i></button>
+                    <button class="btns-three" type="submit">VIEW PLANS &nbsp; <i class="fa-solid fa-book-open"></i></button>
                 </div>
             </div>
         </div>
     </div>
-    <!---------------- Fifth Section End ---------------->
 
-<!---------------- Sixth Section Start ---------------->
-<?php include "./includes/carousel.php"?>
+    <?php include "./includes/carousel.php" ?>
 
 
-<!---------------- Sixth Section End ----------------> 
-
-
-<!---------------- Seventh Section Start ---------------->
-
-
-<!---------------- Seventh Section End ---------------->
-
-
-<!---------------- Eighth Section Start ---------------->
-
-<!---------------- Eighth Section End ---------------->
-
-    <!---------------- Footer Section Start ---------------->
     <?php include "./includes/footer.php" ?>
 </body>
+
 </html>
